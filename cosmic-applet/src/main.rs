ac9eeeebd2feb8b2e86ffd0a2151c0d0529ab98c
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2026-03-28 15:42:35 -0500

    Add Linux COSMIC support

---
+++ cosmic-applet/src/main.rs
@@ -0,0 +1,12 @@
+mod app;
+
+fn main() -> cosmic::iced::Result {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("warn")),
+        )
+        .init();
+
+    cosmic::applet::run::<app::AppModel>(())
+}
